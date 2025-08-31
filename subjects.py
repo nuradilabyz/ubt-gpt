d65de30be8a36fe15ed52d1fe50f1771a3f70824
nuradilabyz <nuradilabyz2006@gmail.com>
2025-08-31 18:50:15 +0500

    Create subjects.py

---
+++ subjects.py
@@ -0,0 +1,36 @@
+SUBJECTS = {
+    "Құқық негіздері": {
+        "assistant_id": "asst_rfV8LBcsVkWveXkvJeLznQZL",
+        "vector_store_id": "vs_689c92f46c6481918819bb833479faed"
+    },
+    "Информатика": {
+        "assistant_id": "asst_NHeMToF2A2rQkyCW1hAcc98p",
+        "vector_store_id": "vs_68a96e6989fc8191bd809a04a3b9262e"
+    },
+    "Қазақ_әдебиеті": {
+        "assistant_id": "asst_WQI6p782I60JIQ5wFaRpscpp",
+        "vector_store_id": "vs_68a9816503d481918df7ad5d636adf77"
+    },
+    "Дүниежүзі тарихы": {
+        "assistant_id": "asst_HXTVe2JcPQOVteAH2vaTL6yy",
+        "vector_store_id": "vs_68a9839f37e08191ad61d66b7f865834"
+    },
+    "Қазақ тілі": {
+        "assistant_id": "asst_UyyzU3SiuA9sYifctwpR4X1G",
+        "vector_store_id": "vs_68a9842055cc8191b18c50a9abf0a887"
+    },
+    "Химия": {
+        "assistant_id": "asst_CDN5PTMSNk7SHHQT0IBlCPtZ",
+        "vector_store_id": "vs_68a984b9919081918e3726352f84b3bf"
+    },
+    "Қазақстан тарихы": {
+        "assistant_id": "asst_KXQFvlA3gXcDYK7R45uVUCoy",
+        "vector_store_id": "vs_68a9870f966c819195becdd8590e966a"
+    },
+    "География": {
+        "assistant_id": "asst_r5okZdEd4vRal023v837k4Yz",
+        "vector_store_id": "vs_68aab608a65c8191a4fa93b4b7fefbaf"
+    }
+}
+
+
